bdace66459cf8fa04f89df12cb1500004e0248a5
Kris Hemenway <krishemenway@gmail.com>
2019-08-30 00:28:08 -0700

    adding app view controller to serve up markup for web

---
+++ OnlineRecLeague.Service/AppViewController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineRecLeague.Service.Leagues
+{
+	[Route("")]
+	[Route("league")]
+	[Route("ladder")]
+	[Route("game")]
+	[Route("sport")]
+	public class AppViewController : ControllerBase
+	{
+		[HttpGet("{*path}")]
+		public ActionResult AppView(string path)
+		{
+			Response.ContentType = "text/html; charset=UTF-8";
+			return Content(View);
+		}
+
+		private const string View = @"
+				<html>
+					<head></head>
+					<body>
+						<div id=""app"" />
+						<script src=""/vendor.js""></script>
+						<script src=""/scripts.js""></script>
+						<script type=""text/javascript"">
+							window.initializeApp();
+						</script>
+					</body>
+				</html>";
+	}
+}
